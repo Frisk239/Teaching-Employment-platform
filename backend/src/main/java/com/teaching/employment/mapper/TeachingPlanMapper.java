697e8227bc7142c3093f4ee67c034a13d09df946
余磊 <a2691218460@outlook.com>
2026-01-19 23:41:20 +0800

    feat: add TeachingPlanMapper interface

---
+++ backend/src/main/java/com/teaching/employment/mapper/TeachingPlanMapper.java
@@ -0,0 +1,15 @@
+package com.teaching.employment.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.teaching.employment.entity.TeachingPlan;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 教学计划Mapper接口
+ *
+ * @author Teaching Employment Platform Team
+ * @since 2026-01-19
+ */
+@Mapper
+public interface TeachingPlanMapper extends BaseMapper<TeachingPlan> {
+}
